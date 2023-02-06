bf9bbc19a0a2ca625b3a7ccf98b90957c99eb48e
rajmohammed57 <54151366+rajmohammed57@users.noreply.github.com>
2023-02-06 13:53:17 +0530

    Update 4.visualisation.R

--- 4.visualisation.R
+++ 4.visualisation.R
@@ -1,78 +1,62 @@
-# Linear Plot
-x <- 1:10
-y <- x^2
-plot(x,y,type="l",main="Linear Plot")
 
 # Installing packages
 install.packages("ggplot2")
+library("ggplot2")
 
-# Scatter Plot
-data(mtcars)
-head(mtcars)
-plot(mtcars$wt,mtcars$mpg,main="Scatter Plot",xlab="Weight",ylab="Mileage")
+# Box Plot on iris
+data(iris)
+boxplot(iris$Sepal.Width,main="Box Plot on Sepal Width",ylab="Sepal Width")
+boxplot(iris$Sepal.Width~iris$Species,main="Box Plot on Sepal Width",ylab="Sepal Width",xlab="Species")
 
 #Scatter Plot on iris
 data(iris)
 plot(iris$Sepal.Length,iris$Sepal.Width,main="Sepal Length vs Width", xlab="Sepal length",ylab="Sepal width")
 
-# Box Plot
-boxplot(mtcars$mpg,main="Box Plot on Mileage",ylab="Mileage")
-
-# Box Plot on iris
-data(iris)
-boxplot(iris$Sepal.Width,main="Box Plot on Sepal Width",ylab="Sepal Width")
-
 # outliers on sample data set
 vec <- c(-140,-10,-2,10,11,50,60,100,102,103,106,109,150,280,301)
 boxplot(vec,main="Outliers Example")
 
-# Histogram
-vec <- c(10,11,12,13,14,20,21,23,30,31,40,49)
-hist(vec,main="Histogram Example",ylab="Frequency",xlab="Bins")
+
 
 #Histogram on MTCARS
 hist(mtcars$mpg,main="Histogram on Mileage",ylab="Frequency",xlab="Mileage")
 
-# Bar Plot
-mon <- c("Jan","Feb","Mar","Apr","May","June")
-sales <- c(1000,1500,1100,1250,3000,600)
-barplot(sales,names.arg=mon,main="Bar Plot(Sales per Month)",xlab="Months",ylab="Sales",border="blue")
 
-#Bar Plot on mtcars
+#Bar Chart on mtcars
+barplot(mtcars$mpg,names.arg=c(row.names(mtcars)),las=3,ylab="Mileage")
 
-barplot(head(mtcars,15)$disp,names.arg=c(row.names(head(mtcars,15))),las=3)
-
-
-# Pie Chart
-favcolor <- c("Red","Blue","Green","White")
-votes <- c(100,80,60,150)
-pie(votes,favcolor,main="Pie Chart(Favorite Colors)")
 
 # Pie Chart on iris
 data(iris)
-
-aggregate(.~Species,data=iris,mean)
-setosa <- nrow(subset(iris,iris$Species == "setosa"))
-setosa
-versicolor <- nrow(subset(iris,iris$Species == "versicolor"))
-versicolor
-virginica <- nrow(subset(iris,iris$Species == "virginica"))
-virginica
-
-pie(c(setosa,versicolor,virginica),c("setosa","versicolor","virginica"),main="Pie Chart(Distribution of Species)")
+mydata<-factor(iris$Species)
+mytable<-table(mydata)
+View(mytable)
+pie(mytable,main="Pie Chart",col=c("Pink","White","Purple"))
 
 
-# The Below Code also works
-library(dplyr)
 
-df = iris
-agg_tbl <- df %>% group_by(Species) %>% 
-  summarise(total_count=n(),
-            .groups = 'drop')
-agg_tbl
 
-# Convert tibble to df
-df2 <- agg_tbl %>% as.data.frame()
-df2
-pie(df2$total_count,df2$Species)
 
+### Sir
+
+
+
+data(iris)
+boxplot(iris$Sepal.Length)
+boxplot(iris$Sepal.Width~iris$Species)
+plot(iris$Petal.Length)
+plot(iris$Petal.Length,pch=20)
+salary<-c(20,30,40,50,200)
+mean(salary)
+median(salary)
+boxplot(salary)
+data(mtcars)
+hist(mtcars$mpg)
+barplot(mtcars$disp)
+
+barplot(mtcars$mpg,main="Barplot for mpg", xlab = "tuples",ylab="mpg",col="pink")
+
+mydata<-factor(iris$Species)
+mytable<-table(mydata)
+View(mytable)
+pie(mytable,main="Pie Chart",col=c("Pink","White","Purple"))
