8f945f9e761f8e04d5fe0337a87d84dee9ede8e6
rajmohammed57 <54151366+rajmohammed57@users.noreply.github.com>
2023-02-06 13:54:10 +0530

    Update 6.Logistic_Regression.R

--- 6.Logistic_Regression.R
+++ 6.Logistic_Regression.R
@@ -1,8 +1,10 @@
 mydata <- read.csv("DataSets\\crashdata.csv")
-head(mydata,30)
+head(mydata)
+nrow(mydata)
 
 mytestdata <- read.csv("DataSets\\crashtestdata.csv")
-head(mytestdata,30)
+head(mytestdata)
+nrow(mytestdata)
 
 str(mydata)
 summary(mydata)
@@ -14,11 +16,13 @@ summary(mydata)
 fit <- glm(formula=mydata$CarType~.,family="binomial",data=mydata)
 fit
 summary(fit)
+
 train <- predict(fit,type="response")
 plot(train)
 
 pred <- predict(fit,newdata=mytestdata,type="response")
 plot(pred)
+
 head(mytestdata)
 mytestdata[pred<=0.5,'Predict'] <- "Hatchback"
 mytestdata[pred>0.5,'Predict'] <- "SUV"
@@ -28,3 +32,30 @@ mytestdata
 install.packages("caret")
 library("caret")
 confusionMatrix(table(mytestdata[,7],mytestdata[,6]),positive="Hatchback")
+
+
+#SIR
+
+mydata<-read.csv("crashdata.csv")
+mytestdata<-read.csv("crashtestdata.csv")
+str(mydata)
+summary(mydata)
+mydata[6]<-as.factor(mydata$CarType)
+str(mydata)
+glm(formula = mydata$CarType~.,family="binomial",data=mydata)
+fit<-glm(formula = mydata$CarType~.,family="binomial",data=mydata)
+fit
+summary(fit)
+train<-predict(fit,type="response")
+plot(train)
+test<-predict(fit,type="response",newdata = mytestdata)
+plot(test)
+mytestdata
+mytestdata[test<=0.5,"Predict"]<-"Hatchback"
+mytestdata[test>=0.5,"Predict"]<-"SUV"
+mytestdata
+install.packages("caret")
+library(caret)
+mytable<-table(mytestdata[,7],mytestdata[,6])
+mytable
+confusionMatrix(mytable,positive = "Hatchback")
