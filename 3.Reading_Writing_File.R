fd43836c911d6c6240ae29bd7f6724b9b394c893
rajmohammed57 <54151366+rajmohammed57@users.noreply.github.com>
2023-02-06 13:52:54 +0530

    Update 3.Reading_Writing_File.R

--- 3.Reading_Writing_File.R
+++ 3.Reading_Writing_File.R
@@ -20,12 +20,41 @@ View(file_csv)
 
 
 # xlsx excel file
-
+# install.packages("readxl")
 library(readxl)
 student_excel <- read_excel("DataSets\\student_xlsx.xlsx")
 View(student_excel)
 
 # Writing csv and txt file
-write.csv(student,"written_csv")
-write.table(student,"written_tabe")
+write.csv(student,"written_csv.csv")
+write.table(student,"written_txt.txt")
+
+
+###Sir
+
+mydata<-read.table('mydataset.txt')
+mydata
+mydata<-read.table('mydataset.txt',header = TRUE)
+mydata
+mydata<-read.table('mydatasetcomma.txt',sep = ',')
+mydata
+mydata<-read.table('mydatasetcomma.txt',sep = ',',header = TRUE)
+mydata
+mydata<-read.csv('mydataset.csv')
+mydata
+mydata<-read.csv('mydataset.csv',nrows = 3)
+mydata
+mydata<-read.table('http://assets.datacamp.com/course/compfin/sbuxPrices.csv',sep = ',',header = T)
+mydata
+install.packages("readxl")
+library(readxl)
+mydata<-read_excel("mydataset.xlsx",sheet = "Sheet1")
+mydata
+
+id<-c(100,101,102)
+sname<-c("Mango","Apple","Berry")
+cost<-c(200,100,300)
+fruits<-data.frame(id,sname,cost)
+fruits
+write.csv(fruits,file = "Fruits")
 
